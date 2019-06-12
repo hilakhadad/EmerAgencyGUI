831c69a0637eede387f558c25ca522e23ef30ba9
hilakhadad <hilakhadad@gmail.com>
2019-06-13 01:07:20 +0300

    bla bka bka

--- src/Objects/Complaint.java
+++ src/Objects/Complaint.java
@@ -1,15 +1,29 @@
 package Objects;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Complaint {
 
-    private String complaintID;
+    private String description;
     private User complainant;
     private User defendant;
+    private String status;
 
-    public Complaint(String complaintID, User complainant, User defendant) {
-        this.complaintID = complaintID;
+    private StringProperty sp_description;
+    private StringProperty sp_complainant;
+    private StringProperty sp_defendant;
+    private StringProperty sp_status;
+
+    public Complaint(String description, User complainant, User defendant,String status) {
+        this.description = description;
         this.complainant = complainant;
         this.defendant = defendant;
+        this.status = status;
+        sp_description= new SimpleStringProperty(description);
+        sp_complainant= new SimpleStringProperty(complainant.getUserName());
+        sp_defendant= new SimpleStringProperty(defendant.getUserName());
+        sp_status= new SimpleStringProperty(status);
     }
 
     public User getComplainant() {
@@ -28,7 +42,23 @@ public class Complaint {
         this.defendant = defendant;
     }
 
-    public String getComplaintID() { return complaintID; }
+    public String getDescription() { return description; }
 
-    public void setComplaintID(String complaintID) { this.complaintID = complaintID; }
+    public void setDescription(String complaintID) { this.description = complaintID; }
+
+    public StringProperty getSP_complaintDescription() {
+        return sp_description;
+    }
+
+    public StringProperty getSP_complainantProperty() {
+        return sp_complainant;
+    }
+
+    public StringProperty getSP_defendantProperty() {
+        return sp_defendant;
+    }
+
+    public StringProperty getSP_statusProperty() {
+        return sp_status;
+    }
 }
